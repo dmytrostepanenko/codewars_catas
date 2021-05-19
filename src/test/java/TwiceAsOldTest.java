760445ceb6a1981457355953c30e6021ceed1cf3
DESKTOP-JVLI2TT\Test <dmytrostepanenkoua@gmail.com>
2021-05-19 20:39:12 +0300

    add TwiceAsOld

---
+++ src/test/java/TwiceAsOldTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TwiceAsOldTest {
+    @Test
+    public void testSomething() {
+        assertEquals(30, TwiceAsOld.TwiceAsOld(30,0));
+        assertEquals(16, TwiceAsOld.TwiceAsOld(30,7));
+        assertEquals(15, TwiceAsOld.TwiceAsOld(45,30));
+
+    }
+}
